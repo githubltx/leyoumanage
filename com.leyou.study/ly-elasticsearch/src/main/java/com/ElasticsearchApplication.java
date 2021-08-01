8797b1b153a05eb9801c1ef771d03f166a30ffc1
githubltx <1753232097@qq.com>
2021-08-01 20:04:21 +0800

    Elasticsearch练习

---
+++ com.leyou.study/ly-elasticsearch/src/main/java/com/ElasticsearchApplication.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: litaixu
+ * @Date: 2021/8/1 - 08 - 01 - 18:17
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class ElasticsearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class);
+    }
+}
+
---
+++ com.leyou.study/ly-elasticsearch/src/main/java/com/pojo/Item.java
@@ -0,0 +1,40 @@
+package com.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+/**
+ * @Auther: litaixu
+ * @Date: 2021/8/1 - 08 - 01 - 18:17
+ * @Description: leyou.com
+ * @version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(indexName = "heima3",type = "docs", shards = 1, replicas = 0)
+public class Item {
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word")
+    private String title; //标题
+
+    @Field(type = FieldType.Keyword)
+    private String category;// 分类
+
+    @Field(type = FieldType.Keyword)
+    private String brand; // 品牌
+
+    @Field(type = FieldType.Double)
+    private Double price; // 价格
+
+    @Field(index = false, type = FieldType.Keyword)
+    private String images; // 图片地址
+}
+
